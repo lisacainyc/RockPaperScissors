70228848fd8315fa2801b90aa5db8d8ff952d1a3
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-01-26 21:27:06 -0500

    Add computer choice and human choice logic

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 function getComputerChoice(){
-    let compChoice = Math.floor(Math.random() * 3);
-    return compChoice;
+    let result = Math.floor(Math.random() * 3);
+    switch(result){
+        case 0:
+            result = "rock";
+            break;
+        case 1:
+            result = "paper"
+            break;
+        case 2:
+            result = "scissor";
+            break;
+    }
+    return result;
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let input = prompt("Enter rock, paper, or scissors");
+    return input;
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
